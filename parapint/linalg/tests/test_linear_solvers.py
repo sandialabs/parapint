af291adcc20be5bddc615ac4b096d01e446c54c5
llueg <llueg@andrew.cmu.edu>
2025-12-16 13:32:25 -0500

    enabled tests

--- parapint/linalg/tests/test_linear_solvers.py
+++ parapint/linalg/tests/test_linear_solvers.py
@@ -1,12 +1,14 @@
-import unittest
-from pyomo.common.dependencies import attempt_import
-import numpy as np
-import scipy.sparse as sps
-from scipy.sparse import coo_matrix, tril
-import parapint
-import pytest
 import os
+import unittest
+
+import numpy as np
+import pytest
+import scipy.sparse as sps
+from pyomo.common.dependencies import attempt_import
 from pyomo.contrib.pynumero.linalg.ma27 import MA27Interface
+from scipy.sparse import coo_matrix, tril
+
+import parapint
 from parapint.linalg.mkl_pardiso import MKLPardisoInterface
 
 ma27_available = MA27Interface.available()
@@ -54,7 +56,7 @@ def get_schur_complement_matrices(n=10, m=5, seed=0):
     A[:n, n:] = B.A
     A[n:, :n] = B.A.T
     A[n:, n:] = D
-    
+
     S = D - B.T @ np.linalg.inv(H) @ B
 
     A = coo_matrix(A)
@@ -153,7 +155,6 @@ class TestLinearSolvers(unittest.TestCase):
         self._test_inertia_computation(solver)
 
 
-@unittest.skip('This does not work yet')
 class TestWrongNonzeroOrdering(unittest.TestCase):
     def _test_solvers(self, solver, use_tril):
         mat = get_base_matrix(use_tril=use_tril)
